efceb270c99e882cf6feb04fa197db6067f5bc12
Nsikak Akpan <builtybykaksy@gmail.com>
2023-11-09 19:24:19 +0100

    grid gap fixed

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -10,7 +10,7 @@ const Question = () => {
         <div className="container">
             <img src={avatarImg} alt="" />
             <h1>Still have questions?</h1>
-            <p>Can't find the answer you're looking for? Please chat to our friendly team.</p>
+            <p>Can't find the answer you're looking for? Please chat with our friendly team.</p>
             <button className='btn btn-primary' onClick={() => navigate('/contact_us')}>Get in Touch</button>
         </div>        
     </section>
