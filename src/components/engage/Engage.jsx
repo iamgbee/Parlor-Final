494b5531317589bd5a4d261951083f0e62db3bb9
Nsikak Akpan <builtybykaksy@gmail.com>
2023-11-09 18:38:19 +0100

    update the  to engage

--- src/components/engage/Engage.jsx
+++ src/components/engage/Engage.jsx
@@ -10,7 +10,8 @@ const Engage = () => {
         <div className="container">
             <h3>Try Parlor Digital Today</h3>
             <h1 className='section__title'>Connect in Real-Time: Meet Your Customers Where They Search Online</h1>
-            <p>Join over 100+ talented creatives to start building with <span>Parlor</span></p>
+            {/* <p>Join over 100+ talented creatives to start building with <span>Parlor</span></p> */}
+            <p>join over 100+ businesses increasing their revenue with <span>Parlor</span></p>
             <div className="btn__group">              
             <button className='btn btn-primary' onClick={() => navigate('/contact_us')}>Contact Us</button>
             </div>
